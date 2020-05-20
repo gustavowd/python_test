9baacd9e3afeba05dbb638792d18f1ad7ecc2389
Gustavo <gustavo.denardin@gmail.com>
2020-05-20 08:19:27 -0300

    Adicionado parse do Instrospect XML

--- dbus.py
+++ dbus.py
@@ -1,31 +1,36 @@
 from pydbus import SessionBus
 from pydbus.generic import signal
 from gi.repository import GLib
+import xml.etree.ElementTree as ET
 
 
 bus = SessionBus()
 
 # Create an object that will proxy for a particular remote object.
 remote_object = bus.get(
-    "br.copel.inversandro.DBusTutorial", # Bus name
-    "/br/copel/inversandro/DBusTutorial" # Object path
+    "br.wdbus.Example", # Bus name
+    "/br/wdbus/Example" # Object path
 )
 
 # Introspection returns an XML document containing information
 # about the methods supported by an interface.
 print ("Introspection data:\n")
-print (remote_object.Introspect()[0])
+print (remote_object.Introspect())
 
 # Get the power management object
 print(remote_object.Sum(5,15))
 
 print(remote_object.Mul(5,15))
 
-print(remote_object.Introspect())
+tree = ET.ElementTree(ET.fromstring(remote_object.Introspect()))
+root = tree.getroot()
+root.tag
+for child in root:
+    print(child.tag, child.attrib)
 
 
 loop = GLib.MainLoop()
-dbus_filter = "/br/copel/inversandro/DBusTutorial"
+dbus_filter = "/br/wdbus/Example"
 
 
 def cb_server_signal_emission(*args):
